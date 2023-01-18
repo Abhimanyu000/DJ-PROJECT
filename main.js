a110d7be5bea0594ca0c9fe1cfb9a95129809691
Abhimanyu000 <89971842+Abhimanyu000@users.noreply.github.com>
2023-01-18 19:35:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 song1="";
 song2="";
 
+LWX="";
+LWY="";
+RWX="";
+RWY="";
+
 function preload(){
     song1=loadSound("music.mp3");
     song2=loadSound("music2.mp3");
@@ -12,8 +17,31 @@ function setup(){
 
     video=createCapture(VIDEO);
     video.hide();
+
+    POSE=ml5.poseNet(video, modelLoaded);
+    POSE.on("pose", gotPoses);
 }
 
 function draw(){
     image(video, 0, 0, 400, 400);
+}
+
+function modelLoaded(){
+    console.log("MODEL LOADED");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+
+        LWX=results[0].pose.leftWrist.x;
+        LWY=results[0].pose.leftWrist.y;
+        RWX=results[0].pose.rightWrist.x;
+        RWY=results[0].pose.rightWrist.y;
+
+        console.log("LWX: "+LWX);
+        console.log("LWY: "+LWY);
+        console.log("RWX: "+RWX);
+        console.log("RWY: "+RWY);
+    }
 }
